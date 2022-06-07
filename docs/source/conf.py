803cff0b80bd500601647136ce398cd82298f794
swirl <swurl@swurl.xyz>
2022-06-07 15:33:30 -0400

    add license and fix conf.py
    Signed-off-by: swirl <swurl@swurl.xyz>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Beak Squad Programming Manual'
+copyright = '(copyleft) 2022 The Beak Squad'
+author = 'Carson Rueter'
 
 release = '0.1'
 version = '0.1.0'
